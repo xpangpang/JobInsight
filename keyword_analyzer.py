9f0666e93a78c74eb59453ea050589faa15508da
xiayy <284883583@qq.com>
2025-02-07 19:20:04 +0800

    Revert "计划开发一个招聘信息爬虫+热词分析工具，主要功能包括："
    This reverts commit ff55dbba6b37a3a18aa0a7a93b911106da891a96.

--- keyword_analyzer.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-import jieba
-
-def analyze_keywords(job_titles):
-    text = ' '.join(job_titles)
-    words = jieba.lcut(text)
-    word_counts = Counter(words)
-    return word_counts.most_common(10)  # 返回前10高频词
--- main.py
+++
@@ -1,4 +0,0 @@
-from job_search_cli import main
-
-if __name__ == "__main__":
-    main()
